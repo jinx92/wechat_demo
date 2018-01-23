e01863354d0fa62555cc2bcb1a4733ffb45f186c
CatStar25 <catstars@126.com>
2018-01-23 18:25:24 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+Page({
+  /*页面的初始数据*/
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    navBar: [
+      { "imgIcon": "../../images/index/all.png", "navText": "首页" },
+      { "imgIcon": "../../images/index/icon-bag.png", "navText": "男装" },
+      { "imgIcon": "../../images/index/icon-gift.png", "navText": "女装" },
+      { "imgIcon": "../../images/index/hot.png", "navText": "盒装" },
+    ],
+    goodsImg: [
+      'https://img20.360buyimg.com/mcoss/jfs/t15442/354/286022146/29720/dfc0fc8e/5a28f102Nc9dd84ea.jpg',
+      'https://img13.360buyimg.com/mcoss/jfs/t14824/352/287189419/43815/fe6da86e/5a28f42cN083843e6.jpg',
+      'https://img20.360buyimg.com/mcoss/jfs/t15073/206/296889070/58770/20b36869/5a28f46bN2d8762f5.jpg',
+      'https://img14.360buyimg.com/mcoss/jfs/t14158/160/1774963556/65437/a1528271/5a28f4a8N505ffd5e.jpg',
+      'https://img13.360buyimg.com/mcoss/jfs/t12307/295/1801417225/43887/d2d99fe3/5a28f298N3d4e2eb3.jpg',
+      'https://img11.360buyimg.com/mcoss/jfs/t16279/21/12262580/35588/1949cea3/5a28f239N57c67c08.jpg',
+    ]
+  }
+})
---
+++ pages/personal/personal.js
@@ -0,0 +1,37 @@
+Page({
+  data: {
+    listData:[{
+      "Icon": "../../images/personal/list_01.png",
+      "text": "推广订单",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_02.png",
+      "text": "我的二维码",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_03.png",
+      "text": "我的订单",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_04.png",
+      "text": "我的优惠券",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_05.png",
+      "text": "收货地址",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_06.png",
+      "text": "个人资料",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_07.png",
+      "text": "排行榜",
+      "nextIcon": "../../images/personal/next.png"
+    },{
+      "Icon": "../../images/personal/list_08.png",
+      "text": "帮助中心",
+      "nextIcon": "../../images/personal/next.png"
+    }]
+  }
+})
